a7f1c06c47d3ec09f61c8a9af31cc6bc7dad5757
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2019-01-05 15:28:09 +0100

    Update source translation

--- src/settings/power/translations/power.ts
+++ src/settings/power/translations/power.ts
@@ -87,160 +87,160 @@
     </message>
 </context>
 <context>
-    <name>Preflet</name>
+    <name>main</name>
     <message>
-        <location filename="../contents/Preflet.qml" line="57"/>
+        <location filename="../contents/main.qml" line="57"/>
         <source>Devices</source>
-        <translation>Devices</translation>
+        <translation type="unfinished">Devices</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="68"/>
+        <location filename="../contents/main.qml" line="68"/>
         <source>Power Saving</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="71"/>
+        <location filename="../contents/main.qml" line="71"/>
         <source>Screen brightness</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="83"/>
+        <location filename="../contents/main.qml" line="83"/>
         <source>Keyboard brightness</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="95"/>
+        <location filename="../contents/main.qml" line="95"/>
         <source>Dim screen when inactive</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="105"/>
+        <location filename="../contents/main.qml" line="105"/>
         <source>Blank screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="111"/>
+        <location filename="../contents/main.qml" line="111"/>
         <source>1 minute</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="112"/>
+        <location filename="../contents/main.qml" line="112"/>
         <source>2 minutes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="113"/>
+        <location filename="../contents/main.qml" line="113"/>
         <source>3 minutes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="114"/>
+        <location filename="../contents/main.qml" line="114"/>
         <source>4 minutes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="115"/>
+        <location filename="../contents/main.qml" line="115"/>
         <source>5 minutes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="116"/>
+        <location filename="../contents/main.qml" line="116"/>
         <source>8 minutes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="117"/>
+        <location filename="../contents/main.qml" line="117"/>
         <source>10 minutes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="118"/>
+        <location filename="../contents/main.qml" line="118"/>
         <source>12 minutes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="119"/>
+        <location filename="../contents/main.qml" line="119"/>
         <source>15 minutes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="120"/>
+        <location filename="../contents/main.qml" line="120"/>
         <source>Never</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="153"/>
+        <location filename="../contents/main.qml" line="153"/>
         <source>Wi-Fi</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="154"/>
+        <location filename="../contents/main.qml" line="154"/>
         <source>Turn off Wi-Fi to save power</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="163"/>
+        <location filename="../contents/main.qml" line="163"/>
         <source>Bluetooth</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="164"/>
+        <location filename="../contents/main.qml" line="164"/>
         <source>Turn off Bluetooth to save power</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="174"/>
+        <location filename="../contents/main.qml" line="174"/>
         <source>Suspend &amp; Power Button</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="177"/>
+        <location filename="../contents/main.qml" line="177"/>
         <source>Automatic suspend</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="186"/>
-        <location filename="../contents/Preflet.qml" line="192"/>
+        <location filename="../contents/main.qml" line="186"/>
+        <location filename="../contents/main.qml" line="192"/>
         <source>On</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="188"/>
+        <location filename="../contents/main.qml" line="188"/>
         <source>When on battery power</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="190"/>
+        <location filename="../contents/main.qml" line="190"/>
         <source>When plugged in</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="195"/>
+        <location filename="../contents/main.qml" line="195"/>
         <source>Off</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="203"/>
+        <location filename="../contents/main.qml" line="203"/>
         <source>When the Power Button is pressed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="208"/>
+        <location filename="../contents/main.qml" line="208"/>
         <source>Ask</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="209"/>
+        <location filename="../contents/main.qml" line="209"/>
         <source>Suspend</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="210"/>
+        <location filename="../contents/main.qml" line="210"/>
         <source>Hibernate</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="211"/>
+        <location filename="../contents/main.qml" line="211"/>
         <source>Nothing</source>
         <translation type="unfinished"></translation>
     </message>
