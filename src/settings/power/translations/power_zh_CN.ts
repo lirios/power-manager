ec8a09f5bf09731ba8af8c61e3fe07bc4a397c30
Liri CI <ci@liri.io>
2019-08-27 02:45:52 +0000

    Automatic merge of Transifex translations
    [ci skip]

--- src/settings/power/translations/power_zh_CN.ts
+++ src/settings/power/translations/power_zh_CN.ts
@@ -1,115 +1,260 @@
 <?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
 <context>
-    <name>Preflet</name>
+    <name>AutomaticSuspendDialog</name>
     <message>
-        <location filename="../contents/Preflet.qml" line="37"/>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="37"/>
+        <source>Automatic Suspend</source>
+        <translation>自动暂停</translation>
+    </message>
+    <message>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="49"/>
+        <source>On Battery Power</source>
+        <translation>在电池电量</translation>
+    </message>
+    <message>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="75"/>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="151"/>
+        <source>Delay</source>
+        <translation>延迟</translation>
+    </message>
+    <message>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="86"/>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="160"/>
+        <source>15 minutes</source>
+        <translation>15分钟</translation>
+    </message>
+    <message>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="87"/>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="161"/>
+        <source>30 minutes</source>
+        <translation>30分钟</translation>
+    </message>
+    <message>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="88"/>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="162"/>
+        <source>45 minutes</source>
+        <translation>45分钟</translation>
+    </message>
+    <message>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="89"/>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="163"/>
+        <source>1 hour</source>
+        <translation>1小时</translation>
+    </message>
+    <message>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="90"/>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="164"/>
+        <source>80 minutes</source>
+        <translation>80分钟</translation>
+    </message>
+    <message>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="91"/>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="165"/>
+        <source>90 minutes</source>
+        <translation>90分钟</translation>
+    </message>
+    <message>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="92"/>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="166"/>
+        <source>100 minutes</source>
+        <translation>100分钟</translation>
+    </message>
+    <message>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="93"/>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="167"/>
+        <source>2 hours</source>
+        <translation>2小时</translation>
+    </message>
+    <message>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="129"/>
+        <source>Plugged In</source>
+        <translation>已插入</translation>
+    </message>
+    <message>
+        <location filename="../contents/AutomaticSuspendDialog.qml" line="129"/>
+        <source>When idle</source>
+        <translation>闲置时</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryListItem</name>
+    <message>
+        <location filename="../contents/BatteryListItem.qml" line="34"/>
+        <source>%1%</source>
+        <translation>%1%</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../contents/main.qml" line="65"/>
         <source>Devices</source>
         <translation>设备</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="48"/>
+        <location filename="../contents/main.qml" line="77"/>
         <source>Power Saving</source>
-        <translation>节能</translation>
+        <translation>省电</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="51"/>
+        <location filename="../contents/main.qml" line="81"/>
+        <source>Screen brightness</source>
+        <translation>屏幕亮度</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="93"/>
+        <source>Keyboard brightness</source>
+        <translation>键盘亮度</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="105"/>
+        <source>Dim screen when inactive</source>
+        <translation>非活动时屏幕变暗</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="115"/>
         <source>Blank screen</source>
-        <translation>空白屏幕</translation>
+        <translation>息屏</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="56"/>
+        <location filename="../contents/main.qml" line="121"/>
         <source>1 minute</source>
-        <translation>1 分钟</translation>
+        <translation>1分钟</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="57"/>
+        <location filename="../contents/main.qml" line="122"/>
         <source>2 minutes</source>
-        <translation>2 分钟</translation>
+        <translation>2分钟</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="58"/>
+        <location filename="../contents/main.qml" line="123"/>
         <source>3 minutes</source>
-        <translation>3 分钟</translation>
+        <translation>3分钟</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="59"/>
+        <location filename="../contents/main.qml" line="124"/>
         <source>4 minutes</source>
-        <translation>4 分钟</translation>
+        <translation>4分钟</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="60"/>
+        <location filename="../contents/main.qml" line="125"/>
         <source>5 minutes</source>
-        <translation>5 分钟</translation>
+        <translation>5分钟</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="61"/>
+        <location filename="../contents/main.qml" line="126"/>
         <source>8 minutes</source>
-        <translation>8 分钟</translation>
+        <translation>8分钟</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="62"/>
+        <location filename="../contents/main.qml" line="127"/>
         <source>10 minutes</source>
-        <translation>10 分钟</translation>
+        <translation>10分钟</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="63"/>
+        <location filename="../contents/main.qml" line="128"/>
         <source>12 minutes</source>
-        <translation>12 分钟</translation>
+        <translation>12分钟</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="64"/>
+        <location filename="../contents/main.qml" line="129"/>
         <source>15 minutes</source>
-        <translation>15 分钟</translation>
+        <translation>15分钟</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="65"/>
+        <location filename="../contents/main.qml" line="130"/>
         <source>Never</source>
-        <translation>永不</translation>
+        <translation>从不</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="71"/>
+        <location filename="../contents/main.qml" line="163"/>
         <source>Wi-Fi</source>
         <translation>Wi-Fi</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="72"/>
+        <location filename="../contents/main.qml" line="164"/>
         <source>Turn off Wi-Fi to save power</source>
-        <translation>关闭 Wi-Fi 以节能</translation>
+        <translation>关闭Wi-Fi进行省电</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="80"/>
+        <location filename="../contents/main.qml" line="173"/>
+        <source>Bluetooth</source>
+        <translation>蓝牙</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="174"/>
+        <source>Turn off Bluetooth to save power</source>
+        <translation>关闭蓝牙进行省电</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="184"/>
         <source>Suspend &amp; Power Button</source>
-        <translation>休眠与电源键</translation>
+        <translation>暂停&amp;电源按钮</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="83"/>
+        <location filename="../contents/main.qml" line="188"/>
         <source>Automatic suspend</source>
-        <translation>自动休眠</translation>
+        <translation>自动暂停</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="86"/>
+        <location filename="../contents/main.qml" line="197"/>
+        <location filename="../contents/main.qml" line="203"/>
+        <source>On</source>
+        <translation>开</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="199"/>
+        <source>When on battery power</source>
+        <translation>电池模式</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="201"/>
+        <source>When plugged in</source>
+        <translation>插电模式</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="206"/>
         <source>Off</source>
         <translation>关</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="92"/>
+        <location filename="../contents/main.qml" line="214"/>
         <source>When the Power Button is pressed</source>
-        <translation>当电源键被按下</translation>
+        <translation>按下电源键</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="97"/>
+        <location filename="../contents/main.qml" line="219"/>
+        <source>Ask</source>
+        <translation>询问</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="220"/>
+        <location filename="../contents/main.qml" line="251"/>
         <source>Suspend</source>
+        <translation>暂停</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="221"/>
+        <location filename="../contents/main.qml" line="252"/>
+        <source>Hibernate</source>
         <translation>休眠</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="98"/>
-        <source>Hibernate</source>
-        <translation>混合休眠</translation>
+        <location filename="../contents/main.qml" line="222"/>
+        <location filename="../contents/main.qml" line="253"/>
+        <source>Hybrid Sleep</source>
+        <translation>混合睡眠</translation>
     </message>
     <message>
-        <location filename="../contents/Preflet.qml" line="99"/>
+        <location filename="../contents/main.qml" line="223"/>
+        <location filename="../contents/main.qml" line="254"/>
         <source>Nothing</source>
-        <translation>无</translation>
+        <translation>空</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="246"/>
+        <source>When the lid is closed</source>
+        <translation>关闭上盖</translation>
     </message>
 </context>
 </TS>
