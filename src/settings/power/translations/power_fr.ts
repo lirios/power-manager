794ef0dc8a139b62fad277699042232191487186
Liri CI <ci@liri.io>
2019-10-29 03:45:06 +0000

    Automatic merge of Transifex translations
    [ci skip]

--- src/settings/power/translations/power_fr.ts
+++ src/settings/power/translations/power_fr.ts
@@ -243,7 +243,7 @@
         <location filename="../contents/main.qml" line="222"/>
         <location filename="../contents/main.qml" line="253"/>
         <source>Hybrid Sleep</source>
-        <translation type="unfinished"/>
+        <translation>Veille hybride</translation>
     </message>
     <message>
         <location filename="../contents/main.qml" line="223"/>
@@ -254,7 +254,7 @@
     <message>
         <location filename="../contents/main.qml" line="246"/>
         <source>When the lid is closed</source>
-        <translation type="unfinished"/>
+        <translation>Lorsque le capot est fermé</translation>
     </message>
 </context>
 </TS>
