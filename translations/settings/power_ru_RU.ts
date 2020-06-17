127aa1afee26c0872eade8ac87e1b712a3054121
Liri CI <ci@liri.io>
2020-06-17 00:26:34 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/settings/power_ru_RU.ts
@@ -0,0 +1,260 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ru_RU" version="2.1">
+<context>
+    <name>AutomaticSuspendDialog</name>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="37"/>
+        <source>Automatic Suspend</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="49"/>
+        <source>On Battery Power</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="75"/>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="151"/>
+        <source>Delay</source>
+        <translation>Отложить</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="86"/>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="160"/>
+        <source>15 minutes</source>
+        <translation>15 минут</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="87"/>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="161"/>
+        <source>30 minutes</source>
+        <translation>30 минут</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="88"/>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="162"/>
+        <source>45 minutes</source>
+        <translation>45 минут</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="89"/>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="163"/>
+        <source>1 hour</source>
+        <translation>1 час</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="90"/>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="164"/>
+        <source>80 minutes</source>
+        <translation>80 минут</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="91"/>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="165"/>
+        <source>90 minutes</source>
+        <translation>90 минут</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="92"/>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="166"/>
+        <source>100 minutes</source>
+        <translation>100 минут</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="93"/>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="167"/>
+        <source>2 hours</source>
+        <translation>2 часа</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="129"/>
+        <source>Plugged In</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/AutomaticSuspendDialog.qml" line="129"/>
+        <source>When idle</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>BatteryListItem</name>
+    <message>
+        <location filename="../../src/settings/power/contents/BatteryListItem.qml" line="34"/>
+        <source>%1%</source>
+        <translation>%1%</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="65"/>
+        <source>Devices</source>
+        <translation>Устройства</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="77"/>
+        <source>Power Saving</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="81"/>
+        <source>Screen brightness</source>
+        <translation>Яркость экрана</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="93"/>
+        <source>Keyboard brightness</source>
+        <translation>Яркость клавиатуры</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="105"/>
+        <source>Dim screen when inactive</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="115"/>
+        <source>Blank screen</source>
+        <translation>Пустой экран</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="121"/>
+        <source>1 minute</source>
+        <translation>1 минута</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="122"/>
+        <source>2 minutes</source>
+        <translation>2 минуты</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="123"/>
+        <source>3 minutes</source>
+        <translation>3 минуты</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="124"/>
+        <source>4 minutes</source>
+        <translation>4 минуты</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="125"/>
+        <source>5 minutes</source>
+        <translation>5 минут</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="126"/>
+        <source>8 minutes</source>
+        <translation>8 минут</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="127"/>
+        <source>10 minutes</source>
+        <translation>10 минут</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="128"/>
+        <source>12 minutes</source>
+        <translation>12 минут</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="129"/>
+        <source>15 minutes</source>
+        <translation>15 минут</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="130"/>
+        <source>Never</source>
+        <translation>Никогда</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="163"/>
+        <source>Wi-Fi</source>
+        <translation>Wi-Fi</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="164"/>
+        <source>Turn off Wi-Fi to save power</source>
+        <translation>Выключить Wi-Fi для сохранения энергии</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="173"/>
+        <source>Bluetooth</source>
+        <translation>Bluetooth</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="174"/>
+        <source>Turn off Bluetooth to save power</source>
+        <translation>Выключить Bluetooth для сохранения энергии</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="184"/>
+        <source>Suspend &amp; Power Button</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="188"/>
+        <source>Automatic suspend</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="197"/>
+        <location filename="../../src/settings/power/contents/main.qml" line="203"/>
+        <source>On</source>
+        <translation>Вкл</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="199"/>
+        <source>When on battery power</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="201"/>
+        <source>When plugged in</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="206"/>
+        <source>Off</source>
+        <translation>Выкл</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="214"/>
+        <source>When the Power Button is pressed</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="219"/>
+        <source>Ask</source>
+        <translation>Спросить</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="220"/>
+        <location filename="../../src/settings/power/contents/main.qml" line="251"/>
+        <source>Suspend</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="221"/>
+        <location filename="../../src/settings/power/contents/main.qml" line="252"/>
+        <source>Hibernate</source>
+        <translation>Гибернация</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="222"/>
+        <location filename="../../src/settings/power/contents/main.qml" line="253"/>
+        <source>Hybrid Sleep</source>
+        <translation>Гибридный сон</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="223"/>
+        <location filename="../../src/settings/power/contents/main.qml" line="254"/>
+        <source>Nothing</source>
+        <translation>Ничего</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/power/contents/main.qml" line="246"/>
+        <source>When the lid is closed</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
